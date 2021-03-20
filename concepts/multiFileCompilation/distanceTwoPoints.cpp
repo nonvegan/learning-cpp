e5f4743f323d28d52c52ee25555a44104c8df71c
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-03-20 22:53:09 +0000

    Update distanceTwoPoints.cpp

--- concepts/multiFileCompilation/distanceTwoPoints.cpp
+++ concepts/multiFileCompilation/distanceTwoPoints.cpp
@@ -6,5 +6,5 @@ using namespace std;
 int main() {
 	int x1 = -3, y1 = 4, x2 = 20, y2 = -10;
 	double distancePoints = distance(x1, y1, x2, y2);
-	cout << "Distance between the points (" + to_string(x1) + ',' + to_string(y1) + "),(" + to_string(x2) + ',' + to_string(y2) + ") is " + to_string(distancePoints) + '.' << endl;
+	cout << "Distance between the points (" + to_string(x1) + ',' + to_string(y1) + ") and (" + to_string(x2) + ',' + to_string(y2) + ") is " + to_string(distancePoints) + '.' << endl;
 }
